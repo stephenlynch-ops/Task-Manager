f710d350cb0ac061ef2595286b504bd367f53e54
Stephen Lynch <stephen.lynch17@icloud.com>
2022-07-30 13:16:26 +0000

    Added add category button

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,7 @@
 // sidenav initialization
 // We got this from the initialization section of the navbar page on materializecss.com
-document.addEventListener('DOMContentLoaded', function() {
-    var sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  // sidenav initialization
+  let sidenav = document.querySelectorAll(".sidenav");
+  M.Sidenav.init(sidenav);
+});
