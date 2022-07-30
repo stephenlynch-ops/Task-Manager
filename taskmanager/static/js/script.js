033d6e8c4472e75757de12b03b33b785a5b9eec0
Stephen Lynch <stephen.lynch17@icloud.com>
2022-07-30 12:53:57 +0000

    Added tasks.html

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+// sidenav initialization
+// We got this from the initialization section of the navbar page on materializecss.com
+document.addEventListener('DOMContentLoaded', function() {
+    var sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+  });
