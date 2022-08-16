32399bef5845532a0b15e7a06ac79448506f3900
Stephen Lynch <stephen.lynch17@icloud.com>
2022-08-16 06:52:22 +0000

    Added final content to project

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,7 +1,20 @@
-// sidenav initialization
-// We got this from the initialization section of the navbar page on materializecss.com
 document.addEventListener("DOMContentLoaded", function() {
   // sidenav initialization
   let sidenav = document.querySelectorAll(".sidenav");
   M.Sidenav.init(sidenav);
+
+  // datepicker initialization
+  let datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  // collapsible initializataion
+  let collapsibles = document.querySelectorAll(".collapsible");
+  M.Collapsible.init(collapsibles);
 });
